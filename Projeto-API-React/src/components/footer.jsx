8d8fed3c55a4a77204bddec1a883d6c7cb630f51
João Mateus <joaomateusjm.453@gmail.com>
2026-06-11 22:01:19 -0300

    Criação do componente footer

---
+++ Projeto-API-React/src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Consulta de API do Brasil — Programação Frontend — UNIVAS
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
